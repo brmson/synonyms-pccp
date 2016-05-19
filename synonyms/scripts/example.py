d7fe7c07ca71b7b31b815a3fb6af7fcaa0e98f43
Petr Baudis <pasky@ucw.cz>
2016-05-19 23:19:55 +0200

    synonyms/scripts/example.py: A more complete example

--- synonyms/scripts/example.py
+++ synonyms/scripts/example.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python3
+#
+# Usage: synonyms/scripts/example.py DICT UMATRIX SMATRIX WORD
+#
+# Example: synonyms/scripts/example.py data_synonyms/nametag.1.LR.dict data_synonyms/nametag.1.LR.ppmi.mat.10000.svd.U.mtx.gz data_synonyms/nametag.1.LR.ppmi.mat.10000.svd.s.mtx.gz manžel
+
+import sys
+sys.path.append('.')
+
 from synonyms.dictionary import Dictionary
 from synonyms.synonyms import SVDModel
 
-dictionary = Dictionary(filename='dictionary_filename')
-# U = ... load U matrix
-# s = ... load s matrix
+
+dictionary = Dictionary(filename=sys.argv[1])
+U = np.load(sys.argv[2], mmap_mode='r')
+s = np.load(sys.argv[3], mmap_mode='r')
 model = SVDModel(U, s, dictionary)
 model.dimensions = 2500
 model.caron_p = 0.25
-synonyms, ids, score = model.get_synonyms('target_word', 10, return_ids=True, return_score=True)
+synonyms, ids, score = model.get_synonyms(sys.argv[4], 10, return_ids=True, return_score=True)
+print(synonyms, ids, score)
